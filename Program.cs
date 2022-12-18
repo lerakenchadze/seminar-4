6084e59d7838441e47cab25464d3561e353d80c2
Lera <valeriya-kenchadze@mail.ru>
2022-12-18 23:07:27 +0300

    add 3 tasks

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿// Напишите программу, которая принимает на вход число (A) и выдает сумму чисел от 1 до A.
+
+// int SummNumbers(int num)
+// {
+//     int summ = 0;
+//     for(int i = 1; i<=num; i++)
+//         summ += i;
+//     return summ;
+// }
+
+// Console.Write("Enter your number: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int summ = SummNumbers(num);
+// Console.WriteLine($"Summ of numbers from 1 to {num} is {summ}");
+
+
+
+
+// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе
+
+// int Chislo(int N)
+// {
+//     if (N == 0)
+//     {
+//         return 1;
+    
+//     }
+//     else 
+//     {
+//         int i = 0;
+//     while (N > 0)
+//     {
+//         i++;
+//         N = N / 10;
+//     }
+//     return i;
+//     }
+    
+// }
+
+// Console.Write("Enter your number: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int res = Chislo(N);
+// Console.WriteLine($"количество цифр равно {res}");
+
+
+
+
+// Напишите программу, которая принимает на вход число (N) и выдает произведение от 1 до N.
+
+// int Fact(int N)
+// {
+//     if (N < 0)
+//     {
+//         N = N * (-1);
+//     }
+//     int f = 1;
+//     for (int i = 2; i <= N; i++)
+//     {
+//         f *= i;
+//     }
+//     return f;
+// }
+
+// Console.Write("Enter your number: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int res = Fact(N);
+// Console.WriteLine($"Факториал числа {N} равен {res}");
+
+
+
